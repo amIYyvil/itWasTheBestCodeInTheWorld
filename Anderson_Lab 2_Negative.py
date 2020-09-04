1b609f6ab29b861ac415c9a63b6861553e52e31f
amIYyvil <70776455+amIYyvil@users.noreply.github.com>
2020-09-04 16:16:17 -0500

    Add files via upload
    You can't make negative cookies (and who would want to?)

---
+++ Anderson_Lab 2_Negative.py
@@ -0,0 +1,33 @@
+#"Cookie Recipe Translator", by Amy Anderson
+#
+#This program is complete and functioning
+#"Cookie Recipe Translator" allows its user
+#to calculate the precise amount of
+#ingredients needed to make any
+#number of cookies they desire.
+
+#Design a constant amount of each ingredient
+#needed to make one cookie
+cups_butter = 1.5/48
+cups_sugar = 1.0/48
+cups_flour = 2.75/48
+
+#Ask the user how many cookies they will be making
+num_cookies = int(input('How many cookies would you like to make? '))
+if num_cookies < 0:
+    print("Can't make negative cookies")
+    exit()
+
+
+#Conveniently deliver a precise amount of each
+#ingredient needed to make the user's amount
+#of cookies
+
+print('To make',num_cookies,'cookies, you will need:')
+
+print(format(num_cookies*cups_butter,'.2f'),'cups of butter')
+
+print(format(num_cookies*cups_sugar,'.2f'),'cups of sugar')
+
+print(format(num_cookies*cups_flour,'.2f'),'cups of flour')
+      
