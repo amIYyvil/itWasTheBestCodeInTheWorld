4fefddad4f7ad1b2c027d41b4cb84b3aab8751ab
Amy Anderson <70776455+amIYyvil@users.noreply.github.com>
2020-10-18 10:53:24 -0500

    Add files via upload

---
+++ Anderson_Lab6.7.py
@@ -0,0 +1,27 @@
+def main():
+    import random
+    
+    try:
+        outfile = open('randnums.txt','w')
+        num_range = int(input('How many random numbers (in range '\
+                          '1-500) would you like to generate? '))
+
+        if num_range >= 1 and num_range <= 500:
+            for count in range(1,num_range+1):
+                number = random.randint(1,count)
+                outfile.write(str(number)+'\n')
+                print(number)
+        else:
+            print('Error! Number should be between 1 and 500.')
+        outfile.close()            
+                        
+    except IOError:
+        print('Something went wrong when '\
+              'opening the file.')
+    except ValueError:
+        print('Error! Please enter a number between 1 and 500')
+    except:
+        print('An error has occurred!')
+    
+            
+main()
