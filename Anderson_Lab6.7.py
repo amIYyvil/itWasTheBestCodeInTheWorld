2378553164d40388deb4f825b80b50bb3b2fa891
Amy Anderson <70776455+amIYyvil@users.noreply.github.com>
2020-10-18 18:21:25 -0500

    Improved upon some things...

--- Anderson_Lab6.7.py
+++ Anderson_Lab6.7.py
@@ -1,17 +1,24 @@
+# This program was written by Amy Anderson
+# It is fully functional
+
+# This program takes input from the user
+# and generates that number of random integers.
+# It then stores these values to a text file
+# called randnums.txt
+
 def main():
     import random
     
     try:
-        
+        outfile = open('randnums.txt','w')
         num_range = int(input('How many random numbers (in range '\
                           '1-500) would you like to generate? '))
 
         if num_range >= 1 and num_range <= 500:
-            outfile = open('randnums.txt','w')
             for count in range(1,num_range+1):
                 number = random.randint(1,count)
                 outfile.write(str(number)+'\n')
-                print(number)
+                
         else:
             print('Error! Number should be between 1 and 500.')
                    
@@ -25,6 +32,8 @@ def main():
     except:
         print('An error has occurred!')
     finally:
-        outfile.close() 
-            
+        outfile.close()
+        print()
+        print('Success! '+ str(num_range)+' random numbers have been '+\
+                      'recorded to the file randnums.txt.')    
 main()
